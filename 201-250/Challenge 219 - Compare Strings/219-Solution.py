b8044fc03fff84ceb952fcdc3dda3ca4d7f94f37
Ryan Moise <ryanjmoise@gmail.com>
2020-08-26 12:03:00 -0400

    Add check for the single character and convert string to lower before check

--- 201-250/Challenge 219 - Compare Strings/219-Solution.py
+++ 201-250/Challenge 219 - Compare Strings/219-Solution.py
@@ -1,15 +1,23 @@
 def compareStrings(inputString, characterCheck):
 
+    lowerString = inputString.lower()
     occurence = 0
-    for elements in inputString:
-        if elements == characterCheck:
-            occurence += 1
 
-    print(occurence)
+    if len(characterCheck) == 0:
+        print("There must be a character to check against.")
+        return
+
+    else:
+        for elements in lowerString:
+            if elements == characterCheck:
+                occurence += 1
+
+        print(occurence)
 
 
-compareStrings("test", "t")
+compareStrings("How many T's do I have", "t")
 compareStrings('oh goodness gracious', 'o')
 compareStrings('howdy, pardner', 'd')
 compareStrings('Incumbent President', 'e')
 compareStrings('', 'z')
+compareStrings('Test', '')
