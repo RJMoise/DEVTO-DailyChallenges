a3f5296719fe44c5d0c29f03692f6320c411bd12
Ryan Moise <ryanjmoise@gmail.com>
2020-08-26 11:54:09 -0400

    Add solution to challenge 219

---
+++ 201-250/Challenge 219 - Compare Strings/219-Solution.py
@@ -0,0 +1,15 @@
+def compareStrings(inputString, characterCheck):
+
+    occurence = 0
+
+    for elements in inputString:
+        if elements == characterCheck:
+            occurence += 1
+
+    print(occurence)
+
+
+compareStrings("test", "t")
+compareStrings('oh goodness gracious', 'o')
+compareStrings('howdy, pardner', 'd')
+compareStrings('Incumbent President', 'e')
